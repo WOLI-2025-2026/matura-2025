71f0bb80102948d3295f178c4c9b6c29b0e5c531
Jakub Grątkiewicz <jakubgr@Mac.lan>
2025-10-30 15:41:36 +0100

    FIxups

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,3 @@
-/*
-## Warunki oceniania
-Ze zględu na działanie testów automatycznych, pliki wynikowe z zadań powinny być zapisywane w osobnych plikach w folderze zgodnie z następującym aliasem 
-
-`wynik{numer-zadania}_{numer-podzadania}.txt`
-
-np. dla zadania 2.1 
-
-`wynik2_1.txt`
-
-Dodatkowo zadania nalezy rozwiazywać w pliku `main.cpp` realizujac zadania poprzez deklaracje kolejnych metod o nazwie i sygnaturze zgodnej ponizej:
-
-`Zadanie{numer-zadania}_{numer-podzadania}()`
-
-np. dla zadnia 2.1
-
-`Zadanie2_1()`
-
-Program powinien realizować zadania w taki sposób aby funkcja wypisywała ten sam stumień wyjściowy do pliku wynikowego, jak równiez na ekran konsoli (standardowy strumień wyjściowy)
-
-np. dla zadnaia 2.1
-```
-cout << result ;
-wynik << result; //wynik jest ofstream do pliku wynik2_1
-```
-*/
-
 #include <iostream>
 int main() {
     std::cout << "Hello, Matura 2025!\n";
